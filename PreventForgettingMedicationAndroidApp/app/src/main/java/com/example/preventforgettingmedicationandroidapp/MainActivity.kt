b7cc7af673556ce7eb4bdbe0413bdf7f82604739
Yoshihisa Saito <anti_viral_drug@yahoo.co.jp>
2025-08-16 08:03:40 +0900

    Show registered medications on main screen

--- PreventForgettingMedicationAndroidApp/app/src/main/java/com/example/preventforgettingmedicationandroidapp/MainActivity.kt
+++ PreventForgettingMedicationAndroidApp/app/src/main/java/com/example/preventforgettingmedicationandroidapp/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.preventforgettingmedicationandroidapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val dao by lazy { MedicationDatabase.getInstance(this).medicationDao() }
+    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,8 +23,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, mutableListOf())
+        findViewById<ListView>(R.id.medication_list).adapter = adapter
+
         findViewById<Button>(R.id.add_medication_button).setOnClickListener {
             startActivity(Intent(this, MedicationRegistrationActivity::class.java))
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val medications = dao.getAll()
+        adapter.clear()
+        adapter.addAll(medications.map { it.name })
+        adapter.notifyDataSetChanged()
     }
 }
